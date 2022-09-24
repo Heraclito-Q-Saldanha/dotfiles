96d65c90c90dbfcfff4b261c40d247ae0923f637
Heráclito <heraclitoqsaldanha@gmail.com>
2022-09-23 22:16:26 -0300

    update

--- lmde5-setup.sh
+++ lmde5-setup.sh
@@ -4,8 +4,6 @@ cp .bashrc ~
 
 sudo apt update -y
 
-sudo apturl apt://mint-meta-codecs?refresh=yes
-
 sudo apt purge -y \
 	mintstick* \
 	gucharmap* \
@@ -54,6 +52,7 @@ sudo apt install -y \
 	git \
 	git-lfs \
 	htop \
+	mint-meta-codecs \
 	papirus-icon-theme
 
 # amd-gpu-fan-daemon
@@ -86,6 +85,12 @@ echo '{
 		"recommendationsUrl": ""
 	}
 }' > ~/.config/VSCodium/product.json
+
+#virtualbox
+sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bullseye contrib" >> /etc/apt/sources.list.d/virtualbox.list
+wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+
+sudo apt update && sudo apt install virtualbox -y
 
 # flatpak
 flatpak install flathub -y com.obsproject.Studio com.valvesoftware.Steam org.kde.kdenlive net.lutris.Lutris com.github.tchx84.Flatseal org.gimp.GIMP org.blender.Blender com.orama_interactive.Pixelorama
