19d04c4148a5ea2706823a092e4bd095a5e94b4c
Heráclito <heraclitoqsaldanha@gmail.com>
2022-09-27 14:54:12 -0300

    update

--- lmde5-setup.sh
+++ lmde5-setup.sh
@@ -89,9 +89,8 @@ echo '{
 }' > ~/.config/VSCodium/product.json
 
 #virtualbox
-sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bullseye contrib" >> /etc/apt/sources.list.d/virtualbox.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bullseye contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
-
 sudo apt update && sudo apt install virtualbox -y
 
 # flatpak
